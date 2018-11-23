53f13d4c97a8d55e86eaa7b03ae2652d1db991e6
Shania Mahmud <smahmud2@binghamton.edu>
2018-11-23 13:52:23 -0500

    hoarsePartition

--- main.cpp
+++ main.cpp
@@ -32,18 +32,48 @@ int partition(vector<pair<int, char>>& arr, int p, int r){
 
 	
 }
-void quickSort(vector<pair<int, char>>& arr,  int p, int r){
+int hoarsPartition(vector<pair<int, char>>& arr, int p, int r){
+	pair<int, char> last=arr[p];
+	int i = p-1;
+	int j = r+1;
+	pair<int, char> other;
+	while( true ){
+		do {
+			j--;
+		}
+		while(!(arr[j].first<=last.first));
+		do {
+			i++;
+		}
+		while(!(arr[i].first>=last.first));
+		if (i<j){
+			other=arr[i];
+			arr[i]=arr[j];
+			arr[j]=other;
+		}
+		else{
+		return j;
+		}
+	}
+	//return j;
+}
+void quickSort(vector<pair<int, char>>& arr,  int p, int r, int arg){
 	int q;
 	if (p<r){
+		if(arg==0){
 		q=partition(arr ,p, r);
-		quickSort(arr, p, q-1);
-		quickSort(arr, q+1, r);
+		}
+		else if(arg==1){
+		q=hoarsPartition(arr, p, r);
+		}
+		quickSort(arr, p, q-1, arg);
+		quickSort(arr, q+1, r, arg);
 	}
 }
 int main(){
 	//vector<pair<int, int>> arr ={(1,2}};
 	vector<pair<int, char>> arr={make_pair(4,'s'),make_pair(2,'s'),make_pair(2,'m'),make_pair(16,'s'),make_pair(13,'s')};
-	quickSort(arr,0, 4);
+	quickSort(arr,0, 4, 1);
 	for (auto x: arr){
 		cerr << x.first << x.second << endl;
 	}
